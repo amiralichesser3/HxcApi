6685af71d2289059a82e4329d43e1deb8ef1ba40
amiralichesser3 <safari.amirali.asa@gmail.com>
2024-01-19 12:21:00 +0330

    ID-5 Make sure the app can communicate with the Mssql docker image and fetch data

--- HxcApi/HxcApi/Program.cs
+++ HxcApi/HxcApi/Program.cs
@@ -1,7 +1,13 @@
 using System.Security.Claims;
 using System.Text.Json.Serialization;
+using HxcApi.Utility;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Data.SqlClient;
 using Microsoft.IdentityModel.Tokens;
+
+
+// Set the CORECLR_GLOBAL_INVARIANT environment variable
+Environment.SetEnvironmentVariable("CORECLR_GLOBAL_INVARIANT", "1");
 
 var builder = WebApplication.CreateSlimBuilder(args);
 builder.Configuration.AddJsonFile("appsettings.Development.Sensitive.json", optional: true, reloadOnChange: true); 
@@ -13,6 +19,7 @@ builder.Services.ConfigureHttpJsonOptions(options =>
 
 string authority = builder.Configuration["Auth0:Domain"]!;
 string audience = builder.Configuration["Auth0:Audience"]!;
+string hxcConString = builder.Configuration["ConnectionStrings:HxcDb"]!;
 builder.Services.AddAuthentication(options =>
     {
         options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -52,6 +59,8 @@ builder.Services.AddCors(options =>
             .AllowAnyHeader());
 });
 
+builder.Services.AddScoped<SqlConnection>(_ => new SqlConnection(hxcConString));
+
 var app = builder.Build();
 
 var userTodos = new Todo[]
@@ -62,16 +71,6 @@ var userTodos = new Todo[]
     new(4, "Clean the bathroom"),
     new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
 };
-
-var organizationTodos = new Todo[]
-{
-    new(1, "Walk the organization dog"),
-    new(2, "Do the organization dishes", DateOnly.FromDateTime(DateTime.Now)),
-    new(3, "Do the organization laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-    new(4, "Clean the organization bathroom"),
-    new(5, "Clean the organization car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-};
-
 var apiMapGroup = app.MapGroup("api/");
 var todosApi = apiMapGroup.MapGroup("user/todos");
 
@@ -84,19 +83,18 @@ todosApi.MapGet("/{id}", (int id) =>
 var organizationTodosApi = apiMapGroup.MapGroup("organization/todos")
     .RequireAuthorization("organization");
 
-organizationTodosApi.MapGet("/", () => organizationTodos);
-organizationTodosApi.MapGet("/{id}", (int id) =>
-    organizationTodos.FirstOrDefault(a => a.Id == id) is { } todo
-        ? Results.Ok(todo)
-        : Results.NotFound());
+organizationTodosApi.MapGet("/", (SqlConnection connection) =>
+    connection.GetTodoData("select * from dbo.Todo"));
 
 app.UseCors("AllowAllOrigins");
 app.UseAuthentication();
 app.UseAuthorization();
+
 app.Run();
 
 public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
 
+[JsonSerializable(typeof(IEnumerable<Todo>))]
 [JsonSerializable(typeof(Todo[]))]
 internal partial class AppJsonSerializerContext : JsonSerializerContext
 {
