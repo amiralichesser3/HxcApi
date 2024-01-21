6598904ce3a27b01052e699ea436db7babc47ff0
amiralichesser3 <safari.amirali.asa@gmail.com>
2024-01-21 20:27:28 +0330

    ID-9 Remove scutor since it has issues with native aot, change dockerfile and use chiseled base docker image and publish the app in PublishAot=true mode

---
+++ HxcApi/HxcApi/DataAccess/DapperImplementation/Todos/Ioc/TodosIocHelper.cs
@@ -0,0 +1,14 @@
+﻿using HxcApi.DataAccess.Contracts.Common.Queries;
+using HxcApi.DataAccess.Contracts.Todos.Queries;
+using HxcApi.DataAccess.DapperImplementation.Todos.Queries;
+
+namespace HxcApi.DataAccess.DapperImplementation.Todos.Ioc;
+
+public static class TodosIocHelper
+{
+    public static void RegisterTodoServices(this IServiceCollection services)
+    {
+        services.AddScoped<IGetOrganizationTodosQueryHandler, GetOrganizationTodosQueryHandler>();
+        services.AddSingleton<IGetOrganizationTodosQueryGenerator, GetOrganizationTodosQueryGenerator>();
+    }
+}
