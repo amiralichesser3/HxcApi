9fb9b8a7e2f5aca4225ed21e3e37c89c477d5511
amiralichesser3 <safari.amirali.asa@gmail.com>
2024-01-24 14:23:00 +0330

    ID-11 Testing the ruleset

---
+++ Tests/HxcApi.UnitTests/HeartbeatTest.cs
@@ -0,0 +1,17 @@
+namespace HxcApi.UnitTests;
+
+[TestClass]
+public class HeartbeatTest
+{
+    [TestMethod]
+    public void AssertOneIsOne()
+    {
+        Assert.AreEqual(1, 1);
+    }
+
+    [TestMethod]
+    public void AssertOneIsTwo()
+    {
+        Assert.AreEqual(1, 2);
+    }
+}
---
+++ Tests/HxcApi.UnitTests/IntegrationTest.cs
@@ -0,0 +1,12 @@
+namespace HxcApi.UnitTests;
+
+[TestClass]
+[TestCategory("IntegrationTest")]
+public class IntegrationTest
+{
+    [TestMethod]
+    public void AssertOneIsTwo()
+    {
+        Assert.AreEqual(1, 2);
+    }
+}
