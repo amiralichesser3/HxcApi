f1f52803932b1d5fad8fb6eac84cbc2cf75a8186
amiralichesser3 <safari.amirali.asa@gmail.com>
2024-01-27 14:22:45 +0330

    ID-12 Add a custom sink to Serilog to log the exceptions and use a global exception handler to handle exceptions based on environment

---
+++ HxcApiJit/Migrations/20240127092620_ErrorLogEventsMigration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HxcApiJit.Migrations
+{
+    /// <inheritdoc />
+    public partial class ErrorLogEventsMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ErrorLogEvents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Exception = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Level = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Timestamp = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClassName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LineNumber = table.Column<int>(type: "int", nullable: false),
+                    KnownExceptionParams = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AppVersion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StackTrace = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ErrorLogEvents", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ErrorLogEvents");
+        }
+    }
+}
